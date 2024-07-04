e4665563ec4990e823fa3f1beff0ff1923eb6785
dmbiam <davidmartinezb1@hotmail.com>
2024-07-03 19:16:54 -0500

    cracion de base en ionic con angular

---
+++ frontend/SPA/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SPA',
+  webDir: 'www'
+};
+
+export default config;
